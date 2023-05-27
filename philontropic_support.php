2990f3e447a7734f9a87a20e8a26744de4658c94
Solomon K <jenish.steps@gmail.com>
2023-05-27 12:20:21 +0530

    Completed Home Menu

--- philontropic_support.php
+++ philontropic_support.php
@@ -3,27 +3,15 @@
    <div class="row pt-5 pb-5">
       <div class="col-lg-9 col-md-9 col-12">
          <h2 class="text-center">PHILONTROPIC <span class="cus-color1">SUPPORT</span></h2>
-         <ul class="mtct-list">
-            <li><i class="fa fa-arrow-right" aria-hidden="true"></i><a class="accordion-toggle" href="#">
-               Free Ambulance Service
-               </a>  
-            </li>
-         </ul>
+         <h5>Free Ambulance Service</h5>
+              
          <div class="text-justify mb-5">
             <p><img src="https://www.motherteresacharities.org/images/Ambulance.jpg" height="180" width="250" style="padding:3px; float:left !important;" alt="Ambulance" class="shadow mb-1 mr-1"><img src="https://www.motherteresacharities.org/images/CSI%20Hospital.jpg" height="180" width="250" style="padding:3px; float:left !important;" alt="Ambulance Neyoor" class="shadow mb-2 mr-2">Mrs.Dr.SP.Singh & Family living in UK contributed a sum of Rs.7,00,000/- in 2007 for the purchase of an ambulance   in her father Shri. Amarjithsingh Kalra’s name for the use of Mother Teresa Charitable Trust. The ambulance is in service since 2007, and now is being used as Mobile Cancer Research Unit in tie up with CSI Mission Hospital, Neyoor,Tamil Nadu under Church of South India. It is beneficial to the general public continuously till now.</p>
-            <ul class="mtct-list">
-               <li><i class="fa fa-arrow-right" aria-hidden="true"></i><a class="accordion-toggle" href="#">
-                  Land near Chennai
-                  </a>  
-               </li>
-            </ul>
-            <p style="height: 160px"><img src="https://www.motherteresacharities.org/images/Andrea.jpg" height="150" width="180" style="padding:3px; float:left !important;" alt="donate" class="shadow ml-2 mb-2"> Ms.Andrea Mendonca, an NRI living in Canada has given a gift of 7 housing plots in Umayalpuram Village, Oragadam near Chennai for the use of Mother Teresa Charitable Trust as meditation center / orphanage. The project is under progress.</p>
-            <ul class="mtct-list">
-               <li><i class="fa fa-arrow-right" aria-hidden="true"></i><a class="accordion-toggle" href="#">
-                  Will in favour of Mother Teresa Charitable Trust
-                  </a>  
-               </li>
-            </ul>
+             <div style="clear:both"></div>
+            <h5>Land near Chennai</h5>
+            <p><img src="https://www.motherteresacharities.org/images/Andrea.jpg" height="150" width="180" style="padding:3px; float:left !important;" alt="donate" class="shadow ml-2 mb-2"> Ms.Andrea Mendonca, an NRI living in Canada has given a gift of 7 housing plots in Umayalpuram Village, Oragadam near Chennai for the use of Mother Teresa Charitable Trust as meditation center / orphanage. The project is under progress.</p>
+             <div style="clear:both"></div>
+            <h5>Will in favour of Mother Teresa Charitable Trust</h5>
             <p>A kind hearted philanthropist Mrs.Kettu Johnson living in Finland has executed a will in favour of Mother Teresa Charitable Trust, her assets viz, Flat, Shares and Bank Balance and other proceeds after her Live.</p>
          </div>
       </div>
