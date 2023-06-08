ef9c478cd3c7704f04e093af0ff403bd60e54571
Ashin Benish <ashinbenish@thesmartsteps.com>
2023-06-08 21:57:46 +0530

    add Mother_Teresa_Forum.php file

---
+++ execution-wings/Mother_Teresa_Forum.php
@@ -0,0 +1,109 @@
+<?php include_once("../master/header.php"); ?>
+<div class="container">
+    <div class="row pt-5 pb-5">
+        <div class="col-lg-9 col-md-9 col-12">
+            <h2 class="text-center">Mother Teresa Forum <span class="cus-color1">- Membership</span></h2>
+            <p class="text-justify">
+                Mother Teresa Forum (MTF), the execution wing of Mother Teresa charitable Trust enrolls members from all citizens of India both male, female or transgenders who is having the willingness to serve the society irrespective of caste, creed, race, religion, region, political affiliation etc. are eligible to enroll as a member. Membership is free of cost. Enrolled members can opt for positions in the MTF, i.e., Nationwide, statewide, District wide, Zonal wide (Taluk), Village level etc. depending on their area of interest or time to spare. “Service to man kind is service to God” which is sacrilegious and should be handled with utmost care and dedication.
+            </p>
+            <p class="text-justify">
+                Mother Teresa Forum expects honesty, sincerity, dedication in the service rendered by our volunteers. We serve under the noble footsteps of the great Mother, Mother Teresa. We expect all our activities transparent and with dedication.
+            </p>
+            <p class="text-justify">
+                On enrolment as member, the volunteers can opt for positions in the prescribed formats. MTF board deserves the right to allow or reject the nominations without assigning any reason.
+            </p>
+            <div class="row">
+                <div class="col-6">
+                    <ul class="mtct-list mt-4">
+                        <a href="">
+                            <li><i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                National Commitees
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="col-6">
+                    <ul class="mtct-list mt-4">
+                        <a href="">
+                            <li><i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                Online Membership
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="col-6">
+                    <ul class="mtct-list mt-4">
+                        <a href="">
+                            <li><i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                State Commitees
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="col-6">
+                    <ul class="mtct-list mt-4">
+                        <a href="">
+                            <li><i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                Offline Membership Form
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="col-6">
+                    <ul class="mtct-list mt-4">
+                        <a href="">
+                            <li><i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                District Comitees
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="col-6">
+                    <ul class="mtct-list mt-4">
+                        <a href="">
+                            <li><i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                Office Bearer Form
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="col-6">
+                    <ul class="mtct-list mt-4">
+                        <a href="">
+                            <li><i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                Other Country Representatives
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <ul class="mtct-list mt-4">
+                                <a href="">
+                                    <li><i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                        By Law-Eng
+                                    </li>
+                                </a>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6">
+                            <ul class="mtct-list mt-4">
+                                <a href="">
+                                    <li><i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                        By Law-Tamil
+                                    </li>
+                                </a>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-12">
+            <?php include_once("../master/side-menu.php"); ?>
+        </div>
+    </div>
+</div>
+<?php include_once("../master/footer.php"); ?>
--- master/header.php
+++ master/header.php
@@ -242,7 +242,7 @@ require $root.'/mtct-v3/master/config.php';?>
                             <li class="dropdown">
                                 <a class="dropdown-toggle" href="<?=$base_url?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mother Teresa Forum</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a href="<?=$base_url?>"> BECOME A MEMBER </a></li>
+                                <li><a href="<?=$base_url?>execution-wings/Mother_Teresa_Forum.php"> BECOME A MEMBER </a></li>
                                 <li><a href="<?=$base_url?>"> ONLINE MEMBERSHIP </a></li>
                                 <li><a href="<?=$base_url?>"> OFFLINE MEMBERSHIP </a></li>
                                 <li><a href="<?=$base_url?>"> NATIONAL COMMITEE </a></li>
