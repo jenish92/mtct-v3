60de78b5c13a021dbffbdd4cdaa3bd51d3bd1c17
Ashin Benish <ashinbenish@thesmartsteps.com>
2023-11-17 00:11:00 +0530

    Added changes in trust board

--- about-us/trust-board.php
+++ about-us/trust-board.php
@@ -1,5 +1,5 @@
 <?php include_once("../master/header.php"); ?>
-<div class="container">
+<!-- <div class="container">
     <div class="row pt-5 pb-5">
         <div class="col-lg-9 col-md-9 col-12">
             <h2 class="text-center">Trust <span class="cus-color1">Board</span></h2>
@@ -41,5 +41,111 @@
             <?php include_once("../master/side-menu.php"); ?>
         </div>
     </div>
+</div> -->
+<div class="container-fluid px-lg-5">
+    <div class="row">
+        <div class="col-lg-9 mt-5">
+            <div class="content-area">
+                <p class="mt-5 mb-3 display-4 content-heading">Trust Board</p>
+                <p>A trustee is a person who is allowed to do certain tasks but not able to gain income on the board of
+                    trustees of an institution that operates for a charity, for the benefit of the general public, A trustee is
+                    bound to act in accordance with the terms of the trusts upon which he or she holds trust property. The
+                    trustee must act impartially between individual beneficiaries as well as between different classes of
+                    beneficiaries, be loyal and administer in the best interest of the beneficiaries. Trust is like a mirror.
+                    Trust takes years to build, seconds to break, and forever to repair. Trust starts with truth and ends with
+                    truth. MTCT is having three members in its trust Board, one Managing trustee and two more Trustees.</p>
+            </div>
+            <div class="content-data">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 d-flex">
+                        <div class="menber-card">
+                            <div class="menber-img">
+                                <img src="https://www.motherteresacharities.org/images/trustees/gk.jpg" alt="">
+                            </div>
+                            <div class="member-details">
+                                <p class="position">Founder & Managing Trustee</p>
+                                <p class="name">Dr.G.K.Dhas</p>
+                                <p class="text">The Managing trustee, Dr. G.K. Dhas, a social activist is the founder of the Trust. 
+                                    The mammoth crowd that followed the adieu procession of Mother Teresa on the 5th of September 1997 
+                                    inspired him to take up the challenge of working for the poor with a motto of “Service to Mankind is 
+                                    Service to God”</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 d-flex">
+                        <div class="menber-card mt-4 mt-lg-0">
+                            <div class="menber-img">
+                                <img src="https://www.motherteresacharities.org/images/trustees/ax.jpg" alt="">
+                            </div>
+                            <div class="member-details">
+                                <p class="position">Trustee</p>
+                                <p class="name">Mr. A.Xavier</p>
+                                <p class="text">The Trustee Mr.A.Xavier an academician is also the co-founder of the trust
+                                     who dedicates himself for the cause of the underprivileged in the society. He is a writer 
+                                     who contributes articles through innovative thoughts and ideas for a change in the society.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 d-flex">
+                        <div class="menber-card mt-4 mt-lg-0">
+                            <div class="menber-img">
+                                <img src="https://www.motherteresacharities.org/images/trustees/vij.jpg" alt="">
+                            </div>
+                            <div class="member-details">
+                                <p class="position">Trustee</p>
+                                <p class="name">Dr. C.Vijayakumar</p>
+                                <p class="text">Another trustee Mr.C.vijayakumar is a philanthropist and a dedicated personality, 
+                                    upholds the high principles of love and service to the society. He involves himself in the activities 
+                                    of the trust and motivates the younger generation to render their helping hands to the needy.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="border my-5 quick-link-panel">
+                <p class="h6 text-center">DONATION</p>
+                <ul>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Online Donation</li>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Through Bank</li>
+                </ul>
+                <p class="h6 text-center">MEMBERSHIP</p>
+                <ul>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Membership Online</li>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Bylaw</li>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Bylaw - Tamil</li>
+                </ul>
+                <p class="h6 text-center">CURRENT PROJECTS</p>
+                <ul>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Free Tailoring Center</li>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Free Computer Center</li>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Free Tuition Center</li>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Free Ambulance Services</li>
+                </ul>
+                <p class="h6 text-center">HELP REQUEST</p>
+                <ul>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Medical Assistance</li>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Educational Assistance</li>
+                </ul>
+                <p class="h6 text-center">BLOOD DONATION</p>
+                <ul>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Donors Registration</li>
+                    <li><i class="fa-solid fa-square-arrow-up-right mr-2"></i>Donors Search</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
+<script>
+    $('#owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        dots: true,
+        nav: true,
+        items: 1,
+        autoplay: true, // Enable autoplay
+        autoplayTimeout: 5000
+    })
+</script>
 <?php include_once("../master/footer.php"); ?>
