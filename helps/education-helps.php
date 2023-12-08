2171dde603b5f051f0b9820fa0e7e86b0d2738a1
Ashin Benish <ashinbenish@thesmartsteps.com>
2023-12-08 21:21:22 +0530

    changes in medical and education helps page

--- helps/education-helps.php
+++ helps/education-helps.php
@@ -2,68 +2,116 @@
 include_once("../master/header.php");
 $medicalHelps = $metaDetails->getHelpRequest(1);
 ?>
-<div class="container">
-    <div class="row pt-5 pb-5">
-        <div class="col-lg-12 col-md-12 col-12">
-            <h2 class="text-center">Education <span class="cus-color1">Helps</span></h2>
-            <div class="text-justify">
+<style>
+    .med-help p {
+        color: #424242;
+    }
 
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <p>"Schools and colleges have reopened. Parents are thriving for school admissions and paying fees. Poverty stricken parents, orphaned children, kids of single parent and children of sick parents suffers a lot to continue their studies. Let us be a helping hand to them. MTCT tries to mobilize funds for their support. Join us in the journey of building a better nation by sponsoring school fees and donating for education. Join with us to help these Children. . .</p>
-                        <div class="alert alert-info p-2">
-                            <h5 class="text-center">"Education is the most powerful weapon which one can use to change the world"</h5>
-                            <h5 class="text-center">"One of the greatest diseases is to be nobody to anybody"</h5>
-                            <h5 class="text-right">- Mother Teresa</h5>
+    .med-help .content-right p {
+        margin-bottom: 5px;
+    }
+
+    .med-help .med-help-img {
+        background-color: #e9f3f7;
+        -webkit-border-top-right-radius: 50px;
+        -webkit-border-bottom-left-radius: 50px;
+        -moz-border-radius-topright: 50px;
+        -moz-border-radius-bottomleft: 50px;
+        border-top-right-radius: 50px;
+        border-bottom-left-radius: 50px;
+        transition: linear 0.4s;
+    }
+
+
+    .med-help span {
+        font-weight: 500;
+        color: black;
+    }
+
+    .med-help .med-help-card {
+        background-color: #e9f3f7;
+        -webkit-border-top-right-radius: 112px;
+        -webkit-border-bottom-left-radius: 112px;
+        -moz-border-radius-topright: 112px;
+        -moz-border-radius-bottomleft: 112px;
+        border-top-right-radius: 112px;
+        border-bottom-left-radius: 112px;
+        transition: linear 0.4s;
+    }
+
+    .med-help>div:nth-child(2n) .med-help-card {
+        -webkit-border-top-right-radius: 0;
+        -webkit-border-bottom-left-radius: 0;
+        -moz-border-radius-topright: 0;
+        -moz-border-radius-bottomleft: 0;
+        border-top-right-radius: 0;
+        border-bottom-left-radius: 0;
+
+        -webkit-border-top-left-radius: 112px;
+        -webkit-border-bottom-right-radius: 112px;
+        -moz-border-radius-topleft: 112px;
+        -moz-border-radius-bottomright: 112px;
+        border-top-left-radius: 112px;
+        border-bottom-right-radius: 112px;
+    }
+
+    .med-help-title {
+        background-color: #ffffff;
+    }
+
+    .med-help-title .med-help-heading {
+        font-size: 30px;
+        font-weight: 700;
+        color: #000000;
+    }
+
+    .med-help-title .med-help-content {
+        text-align: justify;
+    }
+</style>
+<div class="container">
+    <div class="med-help-title py-3 ">
+        <p class="med-help-heading mt-3">Education Helps</p>
+        <p class="med-help-content p-2">
+            "Schools and colleges have reopened. Parents are thriving for school admissions and paying fees.
+            Poverty stricken parents, orphaned children, kids of single parent and children of sick parents suffers a lot to continue their studies.
+            Let us be a helping hand to them. MTCT tries to mobilize funds for their support. Join us in the journey of building a better nation by sponsoring school fees and donating for education. Join with us to help these Children. . .
+        </p>
+        <p class="text-center">"Education is the most powerful weapon which one can use to change the world"
+            "One of the greatest diseases is to be nobody to anybody"
+        </p>
+        <p class="text-right">- Mother Teresa -</p>
+    </div>
+    <div class="row med-help my-5">
+        <?php
+        foreach (json_decode($medicalHelps) as $key => $value) {
+        ?>
+            <div class="col-lg-6 my-4">
+                <div class="med-help-card px-4 py-5 shadow">
+                    <div class="row align-items-center mb-2">
+                        <div class="col-lg-4">
+                            <img src="<?php echo BASE_URL . "img/helps/" . $value->requestor_photo; ?>" alt="" class="img-fluid med-help-img">
                         </div>
+                        <div class="col-lg-8 content-right">
+                            <p><span>Name</span> : <?php echo $value->requestor_name; ?></p>
+                            <p><span>Age</span> : <?php echo $value->requestor_age; ?></p>
+                            <p><span>Gender</span> : <?php echo $value->requestor_gender; ?></p>
+                            <p><span>Place</span> : <?php echo $value->requestor_place; ?></p>
+                            <p><span>Education</span> : <?php echo $value->requestor_ailment; ?></p>
+                            <p><span>Amount requested</span> : <?php echo number_format($value->requestor_amount); ?></p>
+                        </div>
+                    </div>
+                    <div>
+                        <p><span>Family back ground</span> : <?php echo $value->requestor_background; ?>
+                            <a href="<?php echo $value->requestor_profile; ?>">more details...</a>
+                        </p>
                     </div>
                 </div>
-
-                <?php
-                foreach (json_decode($medicalHelps) as $key => $value) {
-                ?>
-                    <!-- New Desing -->
-                    <div class="row shadow border p-lg-2 help-card my-4">
-                        <div class="col-sm-2 d-flex align-items-center justify-content-center">
-                            <img class="img-fluid mt-5 mt-sm-1 rounded" src="<?php echo BASE_URL . "img/helps/" . $value->requestor_photo; ?>" alt="">
-                        </div>
-                        <div class="col-sm-10">
-                            <table class="table table-sm table-bordered help-table mt-3">
-                                <tbody>
-                                    <tr>
-                                        <td class="p-2">Name</td>
-                                        <td class="p-2 font-weight-bold"><?php echo $value->requestor_name; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="p-2">Age</td>
-                                        <td class="p-2"><?php echo $value->requestor_age . " , " . $value->requestor_gender; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="p-2">Education</td>
-                                        <td class="p-2"><?php echo $value->requestor_place; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="p-2">Amount Requested</td>
-                                        <td class="p-2"><i class="fa fa-inr"></i>&nbsp;<?php echo number_format($value->requestor_amount); ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="p-2">Family Background</td>
-                                        <td class="p-2"><?php echo $value->requestor_background; ?><a href="<?php echo $value->requestor_profile; ?>">More Details</a></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                <?php
-                }
-
-                ?>
-
-
             </div>
+        <?php
+        }
 
-
-        </div>
+        ?>
     </div>
 </div>
 <?php include_once("../master/footer.php"); ?>
--- helps/medical-helps.php
+++ helps/medical-helps.php
@@ -2,87 +2,118 @@
 include_once("../master/header.php");
 $medicalHelps = $metaDetails->getHelpRequest(2);
 ?>
-<div class="container">
-    <div class="row pt-5 pb-5">
-        <div class="col-lg-12 col-md-12 col-12">
-            <h2 class="text-center">Medical <span class="cus-color1">Helps</span></h2>
-            <div class="text-justify">
+<style>
+    .med-help p {
+        color: #424242;
+    }
 
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <p>" Health is a complete physical, mental and social wellbeing says – WHO ”. It is one of the fundamental rights of every human being, to attain peace and security. It is the foremost duty of all to protect and safeguard the health of each citizen. Of late COVID-19, the pandemic endangered the health of the elders, the youth and the children worldwide.
-                            Several people died and several got side effects which caused immeasurable pain and agony, physically and economically. It is the foremost duty of the citizen’s at large and helping minded philanthropist to lend a helping hand at the highest possible level to the suffering people to lead their livelihood. Let us help. . .</p>
-                        <h5 class="text-center">- Help people in need -</h5>
+    .med-help .content-right p {
+        margin-bottom: 5px;
+    }
+
+    .med-help .med-help-img {
+        background-color: #e9f3f7;
+        -webkit-border-top-right-radius: 50px;
+        -webkit-border-bottom-left-radius: 50px;
+        -moz-border-radius-topright: 50px;
+        -moz-border-radius-bottomleft: 50px;
+        border-top-right-radius: 50px;
+        border-bottom-left-radius: 50px;
+        transition: linear 0.4s;
+    }
+
+
+    .med-help span {
+        font-weight: 500;
+        color: black;
+    }
+
+    .med-help .med-help-card {
+        background-color: #e9f3f7;
+        -webkit-border-top-right-radius: 112px;
+        -webkit-border-bottom-left-radius: 112px;
+        -moz-border-radius-topright: 112px;
+        -moz-border-radius-bottomleft: 112px;
+        border-top-right-radius: 112px;
+        border-bottom-left-radius: 112px;
+        transition: linear 0.4s;
+    }
+
+    .med-help>div:nth-child(2n) .med-help-card {
+        -webkit-border-top-right-radius: 0;
+        -webkit-border-bottom-left-radius: 0;
+        -moz-border-radius-topright: 0;
+        -moz-border-radius-bottomleft: 0;
+        border-top-right-radius: 0;
+        border-bottom-left-radius: 0;
+
+        -webkit-border-top-left-radius: 112px;
+        -webkit-border-bottom-right-radius: 112px;
+        -moz-border-radius-topleft: 112px;
+        -moz-border-radius-bottomright: 112px;
+        border-top-left-radius: 112px;
+        border-bottom-right-radius: 112px;
+    }
+
+    .med-help-title {
+        background-color: #ffffff;
+    }
+
+    .med-help-title .med-help-heading {
+        font-size: 30px;
+        font-weight: 700;
+        color: #000000;
+    }
+
+    .med-help-title .med-help-content {
+        text-align: justify;
+    }
+</style>
+<div class="container">
+    <div class="med-help-title py-3 ">
+        <p class="med-help-heading mt-3">Medical Helps</p>
+        <p class="med-help-content p-2">
+            " Health is a complete physical, mental and social wellbeing says - WHO ”. It is one of the fundamental
+            rights of every human being, to attain peace and security. It is the foremost duty of all to protect and
+            safeguard the health of each citizen. Of late COVID-19, the pandemic endangered the health of the
+            elders, the youth and the children worldwide. Several people died and several got side effects which
+            caused immeasurable pain and agony, physically and economically. It is the foremost duty of the
+            citizen's at large and helping minded philanthropist to lend a helping hand at the highest possible
+            level to the suffering people to lead their livelihood. Let us help. . .
+        </p>
+        <p class="text-center">- Help people in need -</p>
+    </div>
+    <div class="row med-help my-5">
+        <?php
+        foreach (json_decode($medicalHelps) as $key => $value) {
+        ?>
+            <div class="col-lg-6 my-4">
+                <div class="med-help-card px-4 py-5 shadow">
+                    <div class="row align-items-center mb-2">
+                        <div class="col-lg-4">
+                            <img src="<?php echo BASE_URL . "img/helps/" . $value->requestor_photo; ?>" alt="" class="img-fluid med-help-img">
+                        </div>
+                        <div class="col-lg-8 content-right">
+                            <p><span>Name</span> : <?php echo $value->requestor_name; ?></p>
+                            <p><span>Age</span> : <?php echo $value->requestor_age; ?></p>
+                            <p><span>Gender</span> : <?php echo $value->requestor_gender; ?></p>
+                            <p><span>Place</span> : <?php echo $value->requestor_place; ?></p>
+                            <p><span>Nature of aliment</span> : <?php echo $value->requestor_ailment; ?></p>
+                            <p><span>Amount requested</span> : <?php echo number_format($value->requestor_amount); ?></p>
+                        </div>
+                    </div>
+                    <div>
+                        <p><span>Treatment in</span> : <?php echo $value->treatment_in; ?></p>
+                        <p><span>Family back ground</span> : <?php echo $value->requestor_background; ?>
+                            <a href="<?php echo $value->requestor_profile; ?>">more details...</a>
+                        </p>
                     </div>
                 </div>
-
-                <?php
-                foreach (json_decode($medicalHelps) as $key => $value) {
-                ?>
-                    <!-- new Design -->
-                    <div class="row shadow border p-lg-2 help-card my-4">
-                        <div class="col-sm-2 d-flex align-items-center justify-content-center">
-                            <img class="img-fluid mt-5 mt-sm-1 rounded" src="<?php echo BASE_URL . "img/helps/" . $value->requestor_photo; ?>" alt="">
-                        </div>
-                        <div class="col-sm-10">
-                            <table class="table table-sm table-bordered help-table mt-3">
-                                <tbody>
-                                    <tr>
-                                        <td class="p-2">Name</td>
-                                        <td class="p-2 font-weight-bold"><?php echo $value->requestor_name; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="p-2">Age</td>
-                                        <td class="p-2"><?php echo $value->requestor_age . " , " . $value->requestor_gender; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="p-2">Place</td>
-                                        <td class="p-2"><?php echo $value->requestor_place; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="p-2">Nature of Ailment</td>
-                                        <td class="p-2"><?php echo $value->requestor_ailment; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="p-2">Amount Requested</td>
-                                        <td class="p-2">&nbsp;<i class="fa fa-inr"></i>&nbsp;<?php echo number_format($value->requestor_amount); ?></td>
-                                    </tr>
-                                    <tr class="d-sm-none">
-                                        <td class="p-2">Treatment in</td>
-                                        <td class="p-2"><?php echo $value->treatment_in; ?></td>
-                                    </tr>
-                                    <tr class="d-sm-none">
-                                        <td class="p-2">Family Background</td>
-                                        <td class="p-2"><?php echo $value->requestor_background; ?><a href="<?php echo $value->requestor_profile; ?>">More Details</a></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="p-3 d-none d-sm-block">
-                            <table class="table table-sm table-bordered help-table">
-                                <tbody>
-                                    <tr>
-                                        <td class="p-2">Treatment in</td>
-                                        <td class="p-2"><?php echo $value->treatment_in; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="p-2">Family Background</td>
-                                        <td class="p-2"><?php echo $value->requestor_background; ?><a href="<?php echo $value->requestor_profile; ?>">More Details</a></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                <?php
-                }
-
-                ?>
-
-
             </div>
+        <?php
+        }
 
-
-        </div>
+        ?>
     </div>
 </div>
 <?php include_once("../master/footer.php"); ?>
